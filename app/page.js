8ebc39fad51e1373703d2b39dc16bbf62297a7e0
Anna Sanchez <asa1607@wgu.edu>
2024-08-03 18:59:17 -0400

    added helper functions

--- app/page.js
+++ app/page.js
@@ -1,11 +1,81 @@
-'use client'
+'use client';
 import Image from "next/image";
-import {useState, useEffect} from "react";
-import {firestore} from "@/firebase"
-import { Box } from "@mui/material"
+import { useState, useEffect } from "react";
+import { firestore } from "@/firebase";
+import { Box, Typography } from "@mui/material";
+import { query, collection, getDocs } from "firebase/firestore";
 
 export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState('');
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      });
+    });
+    setInventory(inventoryList);
+  };
+
+   const addItem = async(item) =>{
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()) {
+        const {quantity} = docSnap.data()
+        await setDoc(docRef, {quantity: quantity + 1})
+    }
+    else{
+      await setDoc(docRef, {quantity: 1})
+    }
+
+    await updateInventory()
+  }
+
+
+  const removeItem = async(item) =>{
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      if (quantity === 1){
+        await deleteDoc(docRef) 
+      }
+      else{
+        await setDoc(docRef, {quantity: quantity - 1})
+      }
+    }
+
+    await updateInventory()
+  }
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+   
+
   return (
-    <Box><Typography variant="h1">Inventory Management</Typography></Box>
-  ) 
+    <Box>
+      <Typography variant="h1">Inventory Management</Typography>
+      {inventory.map((item) => {
+        console.log(item);  // Log item here
+        return (
+          <div key={item.name}>
+            <Typography variant="h6">{item.name}</Typography>
+            <Typography variant="body1">{item.count}</Typography>
+          </div>
+        );
+      })}
+    </Box>
+  );
 }
